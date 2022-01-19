34e7e8c245e62db8827ff6afb981ee93ed55f694
Mohammad Burhan <mohammadburhan@Mohammads-Mac-mini.local>
2022-01-20 00:16:40 +0800

    update package and remove safe area view

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import configStore from './redux/config';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 import Main from './screens/main';
 
 const App = () => {
   return (
     <Provider {...{ store: configStore() }}>
-      <SafeAreaView {...{ style: { backgroundColor: Colors.white, flex: 1 } }}>
-        <StatusBar {...{ barStyle: 'dark-content' }} />
-        <Main />
-      </SafeAreaView>
+      <StatusBar {...{ barStyle: 'dark-content' }} />
+      <Main />
     </Provider>
   );
 };
