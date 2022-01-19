f00270fadf1184446596ec97b15815a4fec0abeb
Mohammad Burhan <mohammadburhan@Mohammads-Mac-mini.local>
2022-01-19 10:29:15 +0800

    add marker mapbox

--- src/services/mapbox.js
+++ src/services/mapbox.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
 import Config from 'react-native-config';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 
-const Map = ({ children, coordinates }) => {
+const Map = ({ coordinates }) => {
   MapboxGL.setAccessToken(Config.MAPBOX_API_KEY);
   return (
     <View {...{ style: { height: '100%', width: '100%', flex: 1 } }}>
@@ -18,8 +18,15 @@ const Map = ({ children, coordinates }) => {
             centerCoordinate: coordinates,
           }}
         />
+        <MapboxGL.MarkerView {...{ coordinate: coordinates }}>
+          <Image
+            {...{
+              source: require('../assets/marker.png'),
+              style: { width: 32, height: 32 },
+            }}
+          />
+        </MapboxGL.MarkerView>
       </MapboxGL.MapView>
-      {children}
     </View>
   );
 };
